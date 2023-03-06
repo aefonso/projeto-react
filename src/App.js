ea1c4947ae7aa46aa94134ba39d5c1b3361c13f8
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-03-06 16:43:07 -0300

    feat: criando componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <p>Meu primeiro app</p>
+      <HelloWorld/>
     </div>
   );
 }
