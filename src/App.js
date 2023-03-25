294782d57d45a8b10c2521f243bf8abdaa617b25
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-03-24 21:01:48 -0300

    feat: renderização condicional(if)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Coondicional from './components/Coondicional';
+
 function App() {
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento />
-      <Form />
+      <h1>Renderização Condicional</h1>
+      <Coondicional/>
     </div>
   );
 }
